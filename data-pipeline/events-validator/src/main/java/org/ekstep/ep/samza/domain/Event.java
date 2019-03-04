b7d42b3c7d0dc0962d41ae5ed2fd41e6f812bfbc
Manjunath Davanam <manjunathd@ilimi.in>
2019-03-04 16:02:44 +0530

    Issue SB-10974 feat: Samza job and test cases for the events validator

---
+++ data-pipeline/events-validator/src/main/java/org/ekstep/ep/samza/domain/Event.java
@@ -0,0 +1,132 @@
+package org.ekstep.ep.samza.domain;
+
+import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.ekstep.ep.samza.reader.NullableValue;
+import org.ekstep.ep.samza.reader.Telemetry;
+import org.ekstep.ep.samza.task.TelemetryValidatorConfig;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import com.google.gson.Gson;
+
+public class Event {
+
+    private DateTimeFormatter df = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").withZoneUTC();
+    private final Telemetry telemetry;
+
+    public Event(Map<String, Object> map) {
+        this.telemetry = new Telemetry(map);
+    }
+
+    public Map<String, Object> getMap() {
+        return telemetry.getMap();
+    }
+
+    public String getJson() {
+        Gson gson = new Gson();
+        String json = gson.toJson(getMap());
+        return json;
+    }
+
+    public String getChecksum() {
+
+        String checksum = id();
+        if (checksum != null)
+            return checksum;
+
+        return mid();
+    }
+
+    public String id() {
+        NullableValue<String> checksum = telemetry.read("metadata.checksum");
+        return checksum.value();
+    }
+
+    public String mid() {
+        NullableValue<String> checksum = telemetry.read("mid");
+        return checksum.value();
+    }
+
+    public String eid() {
+        NullableValue<String> eid = telemetry.read("eid");
+        return eid.value();
+    }
+
+    public String pid() {
+        NullableValue<String> pid = telemetry.read("context.pdata.pid");
+        return pid.value();
+    }
+
+    public String schemaName() {
+        String eid = eid();
+        if (eid != null) {
+            return MessageFormat.format("{0}.json", eid.toLowerCase());
+        } else {
+            return "envelope.json";
+        }
+    }
+
+    public String version() {
+        return (String) telemetry.read("ver").value();
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" + "telemetry=" + telemetry + '}';
+    }
+
+    public void markSuccess() {
+        telemetry.addFieldIfAbsent("flags", new HashMap<String, Boolean>());
+        telemetry.add("flags.tv_processed", true);
+        telemetry.add("type", "events");
+    }
+
+    public void markFailure(String error, TelemetryValidatorConfig config) {
+        telemetry.addFieldIfAbsent("flags", new HashMap<String, Boolean>());
+        telemetry.add("flags.tv_processed", false);
+
+        telemetry.addFieldIfAbsent("metadata", new HashMap<String, Object>());
+        if (null != error) {
+            telemetry.add("metadata.tv_error", error);
+            telemetry.add("metadata.src", config.jobName());
+        }
+
+    }
+
+    public void markSkipped() {
+        telemetry.addFieldIfAbsent("flags", new HashMap<String, Boolean>());
+        telemetry.add("flags.tv_skipped", true);
+    }
+
+    public void updateDefaults(TelemetryValidatorConfig config) {
+        String channelString = telemetry.<String>read("context.channel").value();
+        String channel = StringUtils.deleteWhitespace(channelString);
+        if (channel == null || channel.isEmpty()) {
+            telemetry.addFieldIfAbsent("context", new HashMap<String, Object>());
+            telemetry.add("context.channel", config.defaultChannel());
+        }
+        String atTimestamp = telemetry.getAtTimestamp();
+        String strSyncts = telemetry.getSyncts();
+        if (null == atTimestamp && null == strSyncts) {
+            long syncts = System.currentTimeMillis();
+            telemetry.addFieldIfAbsent("syncts", syncts);
+            telemetry.addFieldIfAbsent("@timestamp", df.print(syncts));
+        } else {
+            if (atTimestamp != null) {
+                telemetry.addFieldIfAbsent("syncts", df.parseMillis(atTimestamp));
+            } else if (strSyncts != null) {
+                telemetry.addFieldIfAbsent("@timestamp", strSyncts);
+            }
+        }
+
+    }
+
+    public boolean isSummaryEvent() {
+        NullableValue<String> eid = telemetry.read("eid");
+        return (!eid.isNull() && eid.value().startsWith("ME_"));
+    }
+}
---
+++ data-pipeline/events-validator/src/main/java/org/ekstep/ep/samza/task/TelemetryValidatorConfig.java
@@ -0,0 +1,58 @@
+package org.ekstep.ep.samza.task;
+
+
+import org.apache.samza.config.Config;
+
+public class TelemetryValidatorConfig {
+
+    private final String JOB_NAME = "EventsValidator";
+    private String telemetrySchemaPath;
+    private String summarySchemaPath;
+    private String successTopic;
+    private String failedTopic;
+    private String malformedTopic;
+    private String defaultChannel;
+    private String metricsTopic;
+
+    public TelemetryValidatorConfig(Config config) {
+        System.out.println("Config init:" + config);
+        successTopic = config.get("output.success.topic.name", "telemetry.denorm.valid");
+        failedTopic = config.get("output.failed.topic.name", "telemetry.denorm.failed");
+        malformedTopic = config.get("output.malformed.topic.name", "telemetry.malformed");
+        metricsTopic = config.get("output.metrics.topic.name", "telemetry.pipeline_metrics");
+        defaultChannel = config.get("default.channel", "org.sunbird");
+        telemetrySchemaPath = config.get("telemetry.schema.path", "src/test/resources/druid/telemetry");
+        summarySchemaPath = config.get("summary.schema.path", "src/test/resources/druid/summary");
+        /*System.out.println("telemetrySchemaPath" + telemetrySchemaPath);
+        System.out.println("summarySchemaPath" + summarySchemaPath);
+        System.out.println("defaultChannel" + defaultChannel);*/
+    }
+
+    public String successTopic() {
+        return successTopic;
+    }
+
+    public String failedTopic() {
+        return failedTopic;
+    }
+
+    public String malformedTopic() {
+        return malformedTopic;
+    }
+
+    public String metricsTopic() {
+        return metricsTopic;
+    }
+
+    public String defaultChannel() {
+        return defaultChannel;
+    }
+
+    public String telemetrySchemaPath() { return telemetrySchemaPath; }
+
+    public String summarySchemaPath() { return summarySchemaPath; }
+
+    public String jobName() {
+        return JOB_NAME;
+    }
+}
